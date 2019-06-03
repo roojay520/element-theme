6455275b1925679ef7b5ea5b4cc5e2fbd0806491
roojay <roojay520@gmail.com>
2019-06-03 14:11:24 +0800

    chore: 更新项目配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-empty': 'off',
+    'consistent-return': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': 'off',
+    'global-require': 'off',
+    'import/no-dynamic-require': 'off'
+  },
+};
